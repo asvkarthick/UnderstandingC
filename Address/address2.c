5ca7057b10714bb2dd1bb399f3d309210d2a91ed
Karthick Kumaran <asvkarthick@gmail.com>
2015-10-07 16:59:25 +0530

    Added comments for understanding the code

--- Address/address2.c
+++ Address/address2.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+// Printing the address of a variable
+// Note down the address remains same amount of bits irrespective of the datatype of the variable
+
 int main(void)
 {
     short i;
