b1c30b82f1a0dfac526dac732ad1546127344e5d
Karthick Kumaran <asvkarthick@gmail.com>
2018-08-11 16:30:22 -0700

    Simple obfuscated code

--- Basics/sizeofint.c
+++ Basics/sizeofint.c
@@ -4,6 +4,7 @@ int main(void)
 {
     printf("Size of int = %d\n", sizeof(int));
     printf("Size of int* = %d\n", sizeof(int*));
+    printf("Size of long = %d\n", sizeof(long));
 
     return 0;
 }
